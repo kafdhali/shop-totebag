7bff92a98191864206a6a832621dae33d419b11a
AValenzue <zuellagraphy@gmail.com>
2024-04-30 20:29:22 +0200

    clean files and change footer

---
+++ react/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+      
+            <div className="footer">
+            <a href="https://github.com/avz-kitchen/shop-totebag" >Link to Github</a>
+
+            </div>
+    )
+}
+
+export default Footer
