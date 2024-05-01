59d8807db2cc76468a705134bdb991dd436a44e8
AValenzue <zuellagraphy@gmail.com>
2024-05-01 13:06:48 +0200

    save before changing path

--- react/src/components/HomePage.jsx
+++ react/src/components/HomePage.jsx
@@ -1,18 +1,15 @@
-
-function Home(){
-
-    return (
-<section className="section-hero">
-   <div > <h1>Live life easier,
-with totee.</h1>
-<h3>Sustainable screen rinted totedbags</h3>
-<button>Shop Now</button>
-</div>
-<div className="hero-image">
-    Image Placeholder
-</div>
-</section>)
-
+function Home() {
+    let label ="Shop Now";
+  return (
+    <section className="section-hero">
+      <div>
+        <h1>Live life easier, with totee.</h1>
+        <h3>Sustainable screen rinted totedbags</h3>
+        <button>{label}</button>
+      </div>
+      <div className="hero-image">Image Placeholder</div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
